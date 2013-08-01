da80f67483e0f4059503cb59270fa3a271b5c455
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-02 02:43:50 +0900

    カスタマイズした変数を登録しました
    blink-cursor-mode, column-number-mode, desplay-time-mode,
    show-paren-mode, size-indication-mode, toool-bar-mode

--- personal/custom.el
+++ personal/custom.el
@@ -9,7 +9,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+ '(blink-cursor-mode t)
+ '(column-number-mode t)
  '(custom-enabled-themes (quote (manoj-dark)))
+ '(display-time-mode t)
  '(fci-rule-color "#383838")
  '(graphviz-dot-preview-extension "pdf")
  '(outline-minor-mode-prefix "")
@@ -19,6 +22,9 @@
  '(shell-pop-shell-type (quote ("eshell" "*eshell*" (lambda nil (eshell)))))
  '(shell-pop-universal-key "C-z")
  '(shell-pop-window-height 30)
+ '(show-paren-mode t)
+ '(size-indication-mode t)
+ '(tool-bar-mode nil)
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
