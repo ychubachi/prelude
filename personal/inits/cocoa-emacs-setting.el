303d2447fa1d674e94f26049b2543b7ef7426b1d
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-01 13:27:43 +0900

    Fonts for Mac

--- personal/inits/cocoa-emacs-setting.el
+++ personal/inits/cocoa-emacs-setting.el
@@ -31,42 +31,19 @@
 (mac-set-input-method-parameter
  "com.google.inputmethod.Japanese.base" 'cursor-type 'box)
 
-;; ;; change title
-;; (mac-set-input-method-parameter
-;;  "com.google.inputmethod.Japanese.base" 'title "J")
-
-;; *** know current input source name
-;; (mac-get-current-input-source)
-
-;; *** know current face setting of input source
-;; (mac-get-input-method-parameter input-source-name key)
-
 ;; ================================================================
 ;; Fonts
 ;; ================================================================
-(when (>= emacs-major-version 23)
-  (set-face-attribute 'default nil
-                      :family "monaco"
-                      :height 140)
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0208
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0212
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'mule-unicode-0100-24ff
-   '("monaco" . "iso10646-1"))
-  (setq face-font-rescale-alist
-        '(("^-apple-hiragino.*" . 1.2)
-          (".*osaka-bold.*" . 1.2)
-          (".*osaka-medium.*" . 1.2)
-          (".*courier-bold-.*-mac-roman" . 1.0)
-          (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
-          (".*monaco-bold-.*-mac-roman" . 0.9)
-          ("-cdac$" . 1.3))))
+
+;; |あああああ|
+;; |+-+-+-+-+-|
+;; |imimimimim|
+
+(set-fontset-font (frame-parameter nil 'font)
+                  'unicode
+                  (font-spec :family "Hiragino Maru Gothic ProN")
+                  nil
+                  'append)
+(setq face-font-rescale-alist '(("Hiragino.*" . 1.2)))
 
 ;; cocoa-emacs-setting ends here
