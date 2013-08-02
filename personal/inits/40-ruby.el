6f7836a4d2b0cf49d2115fec23fce2464d1390d4
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-02 23:04:13 +0900

    Clean up

--- personal/inits/40-ruby.el
+++ personal/inits/40-ruby.el
@@ -1,141 +1,63 @@
 ;;; 40-ruby.el --- Ruby development
 ;;; Commentary:
-;
-; http://hmi-me.ciao.jp/wordpress/archives/1295
+;; Preludeのモジュールとして次のものを有効にしています．
+;; - coffee, js, ruby, scss
+
+
+;; S式から正規表現を作成する - by shigemk2
+;; - http://d.hatena.ne.jp/shigemk2/20120419/1334762456
+
+;; EmacsでRubyの開発環境をめちゃガチャパワーアップしたまとめ | Futurismo
+;; http://hmi-me.ciao.jp/wordpress/archives/1295
+
 ;;; Code:
 
-(dolist (package '(flymake-ruby ruby-end rspec-mode))
-  (when (not (package-installed-p package))
-    (package-install package)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; ruby-mode
-;;
+;; ================================================================
+;; Ruby DSLs
+;; ================================================================
 
 (add-to-list 'auto-mode-alist '("Berksfile" . ruby-mode))
 
-;; (add-hook 'ruby-mode-hook
-;;           '(lambda ()
-;;              (abbrev-mode 1)
-;;              (electric-pair-mode t)
-;;              (electric-indent-mode t)
-;;              (electric-layout-mode t)))
+;; ================================================================
+;; outline-minnor-mode
+;; ================================================================
 
-;; (add-hook 'ruby-mode-hook
-;;           '(lambda ()
-;;              (add-to-list 'ruby-encoding-map '(undecided . utf-8))))
+(require 'outline)
+(add-hook 'ruby-mode-hook
+          (function
+           (lambda ()
+             (defun ruby-outline-level ()
+               (or (and (match-string 1)
+                        (or (cdr (assoc (match-string 1) outline-heading-alist))
+                            (- (match-end 1) (match-beginning 1))))
+                   (cdr (assoc (match-string 0) outline-heading-alist))
+                   (- (match-end 0) (match-beginning 0))))
 
-;; ;; endを補間します
-;; (require 'ruby-end)
+             (set (make-local-variable 'outline-level) 'ruby-outline-level)
+
+             (set (make-local-variable 'outline-regexp)
+                  (rx (group (* " "))
+                      bow
+                      (or "begin" "case" "class" "def" "else" "elsif" "end"
+                          "ensure" "if" "module" "rescue" "when" "unless")
+                      eow))
+             (outline-minor-mode))))
 
 ;; ================================================================
-;; flymake-ruby
+;; flymake関係
 ;; ================================================================
+
 (require 'flymake-ruby)
 (add-hook 'ruby-mode-hook 'flymake-ruby-load)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; rspec-mode
-;;
-(require 'rspec-mode)
-(add-hook 'dired-mode-hook 'rspec-dired-mode)
-
-;; 2013-07-26 下記設定をしても，Launcherから起動したときはうまくいきません
-(defadvice rspec-compile (around rspec-compile-around)
-  "Use BASH shell for running the specs because of ZSH issues."
-  (let ((shell-file-name "/bin/bash"))
-    ad-do-it))
-(ad-activate 'rspec-compile)
-
-;; ================================================================
-;; haml
-;; ================================================================
-
-;; パッケージのインストール
-(prelude-require-packages '(haml-mode flymake-haml))
-
-(setq auto-mode-alist
-      (cons (cons "\\.haml$" 'haml-mode) auto-mode-alist))
-(autoload 'haml-mode "haml-mode.el" "haml-mode" t)
-(add-hook 'haml-mode-hook
-          '(lambda ()
-             (setq indent-tabs-mode nil)
-             (define-key haml-mode-map "\C-m" 'newline-and-indent)))
-
-;; flymake-haml
 (require 'flymake-haml)
 (add-hook 'haml-mode-hook 'flymake-haml-load)
 
-;; ================================================================
-;; scss
-;; ================================================================
-
-;; パッケージのインストール
-(dolist (package '(scss-mode flymake-sass))
-  (when (not (package-installed-p package))
-    (package-install package)))
-
-(autoload 'scss-mode "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
-(add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode))
-(add-hook 'scss-mode-hook
-          '(lambda ()
-             (setq indent-tabs-mode nil)
-             (define-key haml-mode-map "\C-m" 'newline-and-indent)))
-(setq scss-compile-at-save nil) ; 保存時自動コンパイルしない
-
-;; ;; sass/scssモードでauto-complete-modeを有効にする
-;; (add-to-list 'ac-modes 'sass-mode)
-;; (add-to-list 'ac-modes 'scss-mode)
-
-;;
-;; flymake-sass
-;;
 (require 'flymake-sass)
 (add-hook 'sass-mode-hook 'flymake-sass-load)
 
-;; ================================================================
-;; javascript
-;; ================================================================
-
-(prelude-require-package 'js2-mode)
-(setq auto-mode-alist
-      (cons (cons "\\.json$" 'js2-mode) auto-mode-alist))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
-;; coffeescript
-;;
-
-;; パッケージのインストール
-(dolist (package '(coffee-mode flymake-coffee))
-  (when (not (package-installed-p package))
-    (package-install package)))
-
-;;
-;; coffeescript
-;;
-(require 'coffee-mode)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-(setq whitespace-action
-      '(auto-cleanup)) ;; automatically clean up bad whitespace
-(setq whitespace-style
-      '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
-(defun coffee-custom ()
-  "coffee-mode-hook"
-  ;; CoffeeScript uses two spaces.
-  (make-local-variable 'tab-width)
-  (set 'tab-width 2))
-
-(add-hook 'coffee-mode-hook 'coffee-custom)
-
-;;
 (require 'flymake-coffee)
 (add-hook 'coffee-mode-hook 'flymake-coffee-load)
-
 
 ;; ================================================================
 ;; 賢いコンパイル
@@ -143,12 +65,34 @@
 
 (prelude-require-package 'smart-compile)
 (require 'smart-compile)
+
 ;; コンパイル前に自動保存
 (setq compilation-ask-about-save nil)
+
 ;; Ruby用キーバインドを設定
 (define-key ruby-mode-map (kbd "C-c c") 'smart-compile)
 (define-key ruby-mode-map (kbd "C-c C-c") (kbd "C-c c C-m"))
+
 ;; コンパイルコマンドを変更
 (add-to-list 'smart-compile-alist '("\\.rb\\'" . "ruby %f"))
 
 ;;; 40-ruby.el ends here
+
+;; (defun ruby-outline-level ()
+;;   "Ruby outline level."
+;;   (or (and (match-string 1)
+;;            (or (cdr (assoc (match-string 1) outline-heading-alist))
+;;                (- (match-end 1) (match-beginning 1))))
+;;       (cdr (assoc (match-string 0) outline-heading-alist))
+;;       (- (match-end 0) (match-beginning 0))))
+
+;; (set (make-local-variable 'outline-level) 'ruby-outline-level)
+
+;; (set (make-local-variable 'outline-regexp)
+;;      (rx (group (* " "))
+;;          bow
+;;          (or "begin" "case" "class" "def" "else" "elsif" "end"
+;;              "ensure" "if" "module" "rescue" "when" "unless")
+;;          eow))
+
+;; (outline-minor-mode)
