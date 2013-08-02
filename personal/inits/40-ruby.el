77d9d1a3ffe8af69ed88b3ba0cd0908c838a29d2
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-02 23:22:04 +0900

    RubyとRspecでoutline-minner-modeする設定をしました

--- personal/inits/40-ruby.el
+++ personal/inits/40-ruby.el
@@ -38,8 +38,27 @@
              (set (make-local-variable 'outline-regexp)
                   (rx (group (* " "))
                       bow
-                      (or "begin" "case" "class" "def" "else" "elsif" "end"
+                      (or "begin" "case" "class" "def" "else" "elsif"
                           "ensure" "if" "module" "rescue" "when" "unless")
+                      eow))
+             (outline-minor-mode))))
+
+(add-hook 'rspec-mode-hook
+          (function
+           (lambda ()
+             (defun rspec-outline-level ()
+               (or (and (match-string 1)
+                        (or (cdr (assoc (match-string 1) outline-heading-alist))
+                            (- (match-end 1) (match-beginning 1))))
+                   (cdr (assoc (match-string 0) outline-heading-alist))
+                   (- (match-end 0) (match-beginning 0))))
+
+             (set (make-local-variable 'outline-level) 'rspec-outline-level)
+
+             (set (make-local-variable 'outline-regexp)
+                  (rx (group (* " "))
+                      bow
+                      (or "context" "describe" "it" "subject")
                       eow))
              (outline-minor-mode))))
 
