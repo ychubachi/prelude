7d4ff18f155ad0571345188e85346f1e8b28a42c
Yoshihide Chubachi <yoshi@chubachi.net>
2013-08-05 11:23:20 +0900

    Clean up 40-ruby.el

--- personal/inits/40-ruby.el
+++ personal/inits/40-ruby.el
@@ -85,33 +85,7 @@
 (prelude-require-package 'smart-compile)
 (require 'smart-compile)
 
-;; コンパイル前に自動保存
-(setq compilation-ask-about-save nil)
-
-;; Ruby用キーバインドを設定
 (define-key ruby-mode-map (kbd "C-c c") 'smart-compile)
 (define-key ruby-mode-map (kbd "C-c C-c") (kbd "C-c c C-m"))
 
-;; コンパイルコマンドを変更
-(add-to-list 'smart-compile-alist '("\\.rb\\'" . "ruby %f"))
-
 ;;; 40-ruby.el ends here
-
-;; (defun ruby-outline-level ()
-;;   "Ruby outline level."
-;;   (or (and (match-string 1)
-;;            (or (cdr (assoc (match-string 1) outline-heading-alist))
-;;                (- (match-end 1) (match-beginning 1))))
-;;       (cdr (assoc (match-string 0) outline-heading-alist))
-;;       (- (match-end 0) (match-beginning 0))))
-
-;; (set (make-local-variable 'outline-level) 'ruby-outline-level)
-
-;; (set (make-local-variable 'outline-regexp)
-;;      (rx (group (* " "))
-;;          bow
-;;          (or "begin" "case" "class" "def" "else" "elsif" "end"
-;;              "ensure" "if" "module" "rescue" "when" "unless")
-;;          eow))
-
-;; (outline-minor-mode)
